73a4e5100a09447cfbc1d423458ee6612c9ed17c
Ben Trans <Ben@d-172-25-44-225.dhcp.virginia.edu>
2017-09-11 14:21:39 -0400

    Created helper2 which contains greeting2 method.

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from helper import greeting
+from helper2 import greeting
 def main():
 	print("Hello")
 	greeting("msg")
+	greeting2("msg")
 
 if __name__ == '__main__': main()
---
+++ helper2.py
@@ -0,0 +1,2 @@
+def greeting2(msg):
+	print("Greeting2" + msg)
